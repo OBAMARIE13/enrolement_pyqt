f53cebccf66299f8d78fe4743b0bcc696d4ffeda
OBAMARIE <obaouattara@gmail.com>
2021-07-23 17:37:26 +0000

    dossier

--- systeme_enrolement/enrolement/widgets/regist.py
+++ systeme_enrolement/enrolement/widgets/regist.py
@@ -2,15 +2,14 @@ import sys
 import os
 import sqlite3
 from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.QtWidgets import QWidget, QMainWindow, QFileDialog
 from PyQt5.uic import loadUiType
-from PyQt5.QtGui import QPixmap
+
 
 
 FORM_CLASS,_ = loadUiType(os.path.join(os.path.dirname("__file__"), "ui/registration.ui"))
 
 
-class Registers(QMainWindow, FORM_CLASS):
+class Registers(QtWidgets.QMainWindow, FORM_CLASS):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -20,6 +19,7 @@ class Registers(QMainWindow, FORM_CLASS):
     def boutons(self):
         self.btn_envoi_2.clicked.connect(self.enregistrement)
         self.btn_photo.clicked.connect(self.get_image_file)
+        self.btn_annuler_2.clicked.connect(self.closeWidget)
 
 
     def enregistrement(self):
@@ -32,6 +32,7 @@ class Registers(QMainWindow, FORM_CLASS):
         nom_pere = self.nom_pere_2.text()
         nom_mere = self.nom_mere_2.text()
         pays = self.pays.text()
+        # label_photo = self.label_photo.picture()
         
        
         if not nom or not prenom or not dates or not nom_pere or not domicile or not nom_mere or not pays or not lieu:
@@ -47,25 +48,26 @@ class Registers(QMainWindow, FORM_CLASS):
         #         QtWidgets.QMessageBox.warning(self, "Error", "Vous etes déja enrolé")
         else:
             QtWidgets.QMessageBox.information(self, "Success", "Message envoyé avec succès")
-            # db = sqlite3.connect(os.path.join(os.path.dirname("__file__"),"database/data.db"))
-            # c = db.cursor()
-            # valeur = (nom, prenom, dates, lieu, domicile, nom_mere, nom_pere, pays)
-            # c.execute("""INSERT INTO contact (nom, prenom, date, lieu, domicile, nom_mere, nom_pere, pays) VALUES (?,?,?,?,?)""", valeur)
-            # db.commit()
+            db = sqlite3.connect(os.path.join(os.path.dirname("__file__"),"database.db"))
+            c = db.cursor()
+            valeur = (nom, prenom, dates, lieu, domicile, nom_mere, nom_pere, pays)
+            c.execute("""INSERT INTO regist (nom, prenom, date, lieu, domicile, nom_mere_2, nom_pere_2, pays) VALUES (?,?,?,?,?,?,?,?)""", valeur)
+            db.commit()
             self.nom.clear()
             self.prenom.clear()
-            self.dates.clear()
+            self.date.clear()
             self.nom_pere_2.clear()
             self.nom_mere_2.clear()
             self.domicile.clear()
             self.pays.clear()
             self.lieu.clear()
+            # self.label_photo.clear()
 
 
  
     def get_image_file(self):
-        file_name,_ = QFileDialog.getOpenFileName(self, 'Open Image File', r"C:\\Users\\Ouattara oba", "Image file(*.jpg *.jpeg *.gif)")
-        self.photo_label.setPixmap(QPixmap(file_name))
+        file_name,_ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open Image File', r"C:\\Users\\Ouattara oba", "Image file(*.jpg *.jpeg *.gif)")
+        self.photo_label.setPixmap(QtGui.QPixmap(file_name))
         self.photo_label.setScaledContents(True)
 
 
@@ -76,11 +78,8 @@ class Registers(QMainWindow, FORM_CLASS):
 #     command = """ SELECT * FROM regist WHERE nom=? AND prenom=? """
 #     resultat = c.execute(command, (noms))
 
+    def closeWidget(self):
+        self.close()
 
 
 
-# def list(self):
-#     db = sqlite3.connect(os.path.join(os.path.dirname("__file__"),"database/data.db"))
-#     c = db.cursor()
-#     command = """ SELECT * FROM regist WHERE nom=? AND prenom=? """
-#     resultat = c.execute(command, (nom, prenom))
